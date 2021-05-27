0f7a22739139ce8ea8b45386d7e98fe342a6cd3e
Caio Nóbile <caio.nobile51@gmail.com>
2021-05-26 21:39:48 -0300

    Soluções da última aula

---
+++ somatorio.py
@@ -0,0 +1,7 @@
+#==================BALAO BRANCO=================
+t = int(input())
+for i in range(t):
+    linha = [int(x) for x in input().split(";")]
+    soma = sum(linha)
+    print(soma, end="" if i == t-1 else "\n")
+#==================BALAO BRANCO=================
